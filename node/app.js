7158875e74a086380ff879afeef6e8a912af0e5a
dtcy <matiasyoon@gmail.com>
2024-02-26 08:44:27 +0900

    render deploy

--- node/app.js
+++ node/app.js
@@ -1,13 +1,13 @@
+// app.js
+
 const express = require('express');
 const app = express();
-const port = 3003;
+const port = process.env.PORT || 3000;
 
-// 루트 경로에 대한 응답
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello from Render!');
 });
 
-// 3000 포트에서 앱 실행
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
