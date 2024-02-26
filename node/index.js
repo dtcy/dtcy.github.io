426bdcae7f1845be325b7db2bad83f86845c4725
dtcy <matiasyoon@gmail.com>
2024-02-26 20:37:02 +0900

    render

--- node/index.js
+++ node/index.js
@@ -1,13 +1,1 @@
-// app.js
-
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hello from Render!');
-});
-
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
-});
+console.log('im node');
