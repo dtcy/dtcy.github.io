5f2d28a6c8bf405ba1d7f5a487b45e2f7853bb42
dtcy <matiasyoon@gmail.com>
2024-02-27 17:26:52 +0900

    node

--- node/index.js
+++ node/index.js
@@ -1,1 +1,17 @@
-console.log('im node');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// 미들웨어 등록
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// 라우트 설정
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// 서버 실행
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
