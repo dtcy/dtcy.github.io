eb32bbe6fbe50923778ca7a991158e8fda46f683
youngmatias <matiasyoon@gmail.com>
2024-03-04 18:23:11 +0900

    fex

--- next/projects/app/components/tictactoe/Square.tsx
+++ next/projects/app/components/tictactoe/Square.tsx
@@ -1,7 +1,13 @@
+// import React from "react";
+// import { value } from "./page";
+// import { useState, useRef } from "react";
+// export default function Square({ value }) {
+//   console.log(value);
+//   return <div className="square">{value}</div>;
+// }
+
 import React from "react";
-import { value } from "./page";
-import { useState, useRef } from "react";
-export default function Square({ value }) {
-  console.log(value);
-  return <div className="square">{value}</div>;
+
+export default function Square() {
+  return <div>Square</div>;
 }
